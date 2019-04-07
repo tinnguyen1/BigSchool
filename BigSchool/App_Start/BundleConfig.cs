ffc9e3cd2b03ab38684e0a9890301788d94de9d4
= <=>
2019-04-07 12:16:39 +0700

    Update

--- BigSchool/App_Start/BundleConfig.cs
+++ BigSchool/App_Start/BundleConfig.cs
@@ -8,8 +8,8 @@ namespace BigSchool
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/add/coursesController.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -21,7 +21,8 @@ namespace BigSchool
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -36,6 +36,23 @@ namespace BigSchool.Controllers
 
             return Ok();
         }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var attendance = _dbContext.Attendances
+                .SingleOrDefault(a => a.AttendeeId == userId && a.CourseId == id);
+
+            if (attendance == null)
+                return NotFound();
+
+            _dbContext.Attendances.Remove(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok(id);
+        }
         /*
         // GET: Attendances
         public ActionResult Index()
